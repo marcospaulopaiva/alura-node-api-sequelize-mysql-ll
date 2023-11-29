e958721a6f91aa376612458ad23cc61575b5cd29
marcospaulopaiva <marcos.poo@gmail.com>
2023-11-29 14:37:30 -0300

    Aula 2 - Escopo de modelo e validações

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,11 +1,34 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if(dado.length <= 3) throw new Error('O nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo e-mail inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
-  }, { paranoid: true })
+  }, { 
+    paranoid: true,
+    defaultScope: {
+      where: {ativo: true}    
+    },
+    scopes: {
+      todos: { where: {} }
+    }
+  })
   Pessoas.associate = function(models) {
     Pessoas.hasMany(models.Turmas, {
       foreignKey: 'docente_id'
